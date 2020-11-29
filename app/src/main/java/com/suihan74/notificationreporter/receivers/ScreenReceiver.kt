d2cf77594c9d3964daad4c6efaf255f432072741
suihan <suihan742@gmail.com>
2020-11-29 21:13:32 +0900

    purge unnecessary lines

--- app/src/main/java/com/suihan74/notificationreporter/receivers/ScreenReceiver.kt
+++ app/src/main/java/com/suihan74/notificationreporter/receivers/ScreenReceiver.kt
@@ -1,17 +1,9 @@
 package com.suihan74.notificationreporter.receivers
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import android.util.Log
-import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat
-import com.suihan74.notificationreporter.R
 import com.suihan74.notificationreporter.scenes.lockScreen.LockScreenActivity
 
 /**
@@ -23,11 +15,7 @@ class ScreenReceiver : BroadcastReceiver() {
 
         when (intent?.action) {
             Intent.ACTION_SCREEN_OFF -> {
-                val lockScreenIntent = Intent().also {
-                    it.setClassName(
-                        "com.suihan74.notificationreporter",
-                        "com.suihan74.notificationreporter.scenes.lockScreen.LockScreenActivity"
-                    )
+                val lockScreenIntent = Intent(context, LockScreenActivity::class.java).also {
                     it.flags = Intent.FLAG_ACTIVITY_NEW_TASK
                 }
                 context.startActivity(lockScreenIntent)
