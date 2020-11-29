0b05ae9d687c3782ec248ea4902413ec2692e869
suihan <suihan742@gmail.com>
2020-11-29 21:28:28 +0900

    modify NotificationService to start LockScreenActivity when any notification are posted

--- app/src/main/java/com/suihan74/notificationreporter/services/NotificationService.kt
+++ app/src/main/java/com/suihan74/notificationreporter/services/NotificationService.kt
@@ -1,8 +1,11 @@
 package com.suihan74.notificationreporter.services
 
+import android.content.Intent
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
+import android.util.Log
 import com.suihan74.notificationreporter.Application
+import com.suihan74.notificationreporter.scenes.lockScreen.LockScreenActivity
 
 /**
  * デバイスのすべての通知発生を検知するサービス
@@ -13,5 +16,12 @@ class NotificationService : NotificationListenerService() {
 
         val repository = Application.instance.notificationRepository
         repository.existUnreadNotifications.value = sbn != null
+
+        val intent = Intent(applicationContext, LockScreenActivity::class.java).also {
+            it.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        applicationContext.startActivity(intent)
+
+        Log.i("Notification", "notification posted")
     }
 }
