d40636dbda737485c346042893d9d922a9dc2ddf
suihan <suihan742@gmail.com>
2020-11-29 22:09:39 +0900

    add a button for posting dummy notifications

--- app/src/main/java/com/suihan74/notificationreporter/scenes/preferences/PreferencesActivity.kt
+++ app/src/main/java/com/suihan74/notificationreporter/scenes/preferences/PreferencesActivity.kt
@@ -1,12 +1,23 @@
 package com.suihan74.notificationreporter.scenes.preferences
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.getSystemService
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.suihan74.notificationreporter.R
 import com.suihan74.notificationreporter.databinding.ActivityPreferencesBinding
 import com.suihan74.notificationreporter.scenes.lockScreen.LockScreenActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * 設定画面
@@ -24,5 +35,44 @@ class PreferencesActivity : AppCompatActivity() {
             val intent = Intent(this, LockScreenActivity::class.java)
             startActivity(intent)
         }
+
+        binding.notifyButton.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.Main) {
+                delay(5_000L)
+                notifyDummy()
+            }
+        }
+    }
+
+    /** ダミーの通知を発生させる */
+    private fun notifyDummy() {
+        val channelId = "DummyNotificationChannel"
+        val notificationId = 334
+
+        createNotificationChannel(channelId)
+
+        val builder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("dummy")
+            .setContentText("dummy")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(notificationId, builder.build())
+        }
+    }
+
+    /** 通知チャンネルを作成する */
+    private fun createNotificationChannel(channelId: String) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return
+
+        val name = "dummy"
+        val description = "dummy notifications for test"
+        val channel = NotificationChannel(channelId, name, NotificationManager.IMPORTANCE_DEFAULT).also {
+            it.description = description
+        }
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
