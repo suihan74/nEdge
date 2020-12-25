f6438c4b65698cf57a526e12f6c71b0ac62e6019
suihan <suihan742@gmail.com>
2020-12-26 00:29:36 +0900

    add extensions in order to show/hide inputMethod to Activity

---
+++ utilities/src/main/java/com/suihan74/utilities/extensions/Activity.kt
@@ -0,0 +1,44 @@
+@file:Suppress("unused")
+
+package com.suihan74.utilities.extensions
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+
+/**
+ * キーボードを表示して対象にフォーカスする
+ */
+fun Activity.showSoftInputMethod(
+    targetView: View,
+    softInputMode: Int? = WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE
+) {
+    if (softInputMode != null) {
+        window?.setSoftInputMode(softInputMode)
+    }
+
+    targetView.requestFocus()
+    (getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager)?.run {
+        showSoftInput(targetView, 0)
+    }
+}
+
+/**
+ * キーボードを隠して入力対象のビューをアンフォーカスする
+ */
+fun Activity.hideSoftInputMethod(focusTarget: View? = null) : Boolean {
+    val windowToken = window.decorView.windowToken
+    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+    val result = imm?.hideSoftInputFromWindow(windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+    currentFocus?.clearFocus()
+
+    (focusTarget ?: window.decorView.rootView)?.let { target ->
+        target.isFocusable = true
+        target.isFocusableInTouchMode = true
+        target.requestFocus()
+    }
+
+    return result ?: false
+}
