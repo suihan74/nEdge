a7b11b19c59006e317e981938ff4e497893b5865
suihan <suihan742@gmail.com>
2021-01-04 18:14:54 +0900

    fix the package name

--- app/src/main/java/com/suihan74/notificationreporter/scenes/preferences/dataBinding/ViewPager2.kt
+++ app/src/main/java/com/suihan74/notificationreporter/scenes/preferences/dataBinding/ViewPager2.kt
@@ -1,4 +1,4 @@
-package com.suihan74.utilities.dataBinding
+package com.suihan74.notificationreporter.scenes.preferences.dataBinding
 
 import androidx.databinding.BindingAdapter
 import androidx.databinding.InverseBindingAdapter
