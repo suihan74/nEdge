5171924700f17cf3403b25f92ae670bd9af98767
suihan <suihan742@gmail.com>
2021-01-10 22:48:41 +0900

    purge unnecessary lines

--- app/src/main/java/com/suihan74/notificationreporter/services/NotificationService.kt
+++ app/src/main/java/com/suihan74/notificationreporter/services/NotificationService.kt
@@ -1,6 +1,5 @@
 package com.suihan74.notificationreporter.services
 
-import android.app.Notification
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import com.suihan74.notificationreporter.Application
@@ -13,16 +12,6 @@ import kotlinx.coroutines.launch
 class NotificationService : NotificationListenerService() {
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
         super.onNotificationPosted(sbn)
-
-        // TODO: blacklist
-        if (sbn?.packageName == "com.android.systemui") {
-            return
-        }
-
-        // ロック画面に表示しない通知を除外する
-        if (sbn?.notification?.visibility == Notification.VISIBILITY_SECRET) {
-            return
-        }
 
         Application.instance.let { app -> app.coroutineScope.launch {
             app.notificationRepository.pushNotification(sbn)
